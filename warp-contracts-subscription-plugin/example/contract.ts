40756dac32dd6054a54164e422e6e383e1344431
ppe <peter@redstone.finance>
2022-11-16 15:02:22 +0100

    feat: subscription plugin example usage

---
+++ warp-contracts-subscription-plugin/example/contract.ts
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+import {LoggerFactory, WarpFactory} from "warp-contracts";
+import {InteractionMessage, StateUpdatePlugin, WarpSubscriptionPlugin} from "../src/plugin";
+
+LoggerFactory.INST.logLevel('none');
+LoggerFactory.INST.logLevel('info', 'subscription-example');
+LoggerFactory.INST.logLevel('debug', 'WarpSubscriptionPlugin');
+const logger = LoggerFactory.INST.create('subscription-example');
+
+class CustomSubscriptionPlugin extends WarpSubscriptionPlugin<void> {
+  async process(input: InteractionMessage): Promise<void> {
+    logger.info('From custom plugin', input);
+    // process the new message;
+  }
+}
+
+async function main() {
+  const contractTxId = 'Ws9hhYckc-zSnVmbBep6q_kZD5zmzYzDmgMC50nMiuE';
+  const warp = WarpFactory.forMainnet();
+  warp.use(new StateUpdatePlugin(contractTxId, warp));
+  warp.use(new CustomSubscriptionPlugin(contractTxId, warp));
+  let wallet = await warp.generateWallet();
+
+  const contract = warp
+    .contract(contractTxId)
+    .connect(wallet.jwk);
+
+  await contract.writeInteraction({
+    function: 'vrf'
+  }, {vrf: true});
+
+}
+
+main().catch((e) => console.error(e));
--- warp-contracts-subscription-plugin/src/plugin.ts
+++ warp-contracts-subscription-plugin/src/plugin.ts
@@ -47,15 +47,36 @@ export abstract class WarpSubscriptionPlugin<R> implements WarpPlugin<Interactio
 export class StateUpdatePlugin<State> extends WarpSubscriptionPlugin<SortKeyCacheResult<EvalStateResult<State>>> {
   async process(input: InteractionMessage): Promise<SortKeyCacheResult<EvalStateResult<State>>> {
     const lastStoredKey = (await this.warp.stateEvaluator.latestAvailableState(this.contractTxId))?.sortKey;
+
+    /**
+     * we need to check whether the locally cached state is at the sort key that is the previous sort key
+     * for the new interaction from the "input".
+     * That's why each new message contains the 'lastSortKey' field.
+     *
+     * If the local state is cached at 'lastSortKey' - it's safe to use the new interaction to update
+     * the local state directly.
+     *
+     * If the local state cache is cached add 'earlier' sort key - we need to ask the Warp Gateway for the missing
+     * interactions.
+     */
+
+    let result: SortKeyCacheResult<EvalStateResult<State>>;
     if (lastStoredKey?.localeCompare(input.lastSortKey) === 0) {
       this.logger.debug('Safe to use new interaction.', input.sortKey);
-      return await this.warp.contract<State>(this.contractTxId).readStateFor([input.interaction]);
+      result = await this.warp.contract<State>(this.contractTxId).readStateFor([input.interaction]);
     } else {
-      this.logger.debug('Unsafe to use new interaction.', {
+      this.logger.debug('Unsafe to use new interaction - reading the state via gateway', {
         lastSortKey: input.lastSortKey,
         localCache: lastStoredKey
       });
-      return await this.warp.contract<State>(this.contractTxId).readState();
+      result =  await this.warp.contract<State>(this.contractTxId).readState();
     }
+
+    this.logger.debug('State updated', {
+      sortKey: result.sortKey,
+      state: result.cachedValue.state
+    });
+    
+    return result;
   }
 }
