a7eb5baf22afc15a5167bcf8ab7699c0b7fe3ee8
koonopek <konopek68@gmail.com>
2022-12-27 12:36:46 +0100

    Add node signer (#4)
    * feat: expose common function to build new evm compatible signers
    
    * refactor: export common method for preparing tx for singining
    
    * feat: Add server evm signer

---
+++ warp-contracts-plugin-signature/src/server/evm/evmSignature.ts
@@ -0,0 +1,19 @@
+import Transaction from "arweave/node/lib/transaction";
+import { Signer } from "ethers";
+import { attachSignature, prepareTransaction } from "../common";
+
+export const attachEvmTags = (tx: Transaction) => {
+    tx.addTag('Signature-Type', 'ethereum');
+    tx.addTag('Nonce', Date.now().toString());
+};
+
+export const evmSignature = (signer: Signer) => async (tx: Transaction) => {
+    const signerAddress = await signer.getAddress();
+
+    attachEvmTags(tx);
+    const toSign = prepareTransaction(tx, signerAddress);
+
+    const signature = await signer.signMessage(toSign);
+
+    await attachSignature(tx, signature);
+};
