fba1d72b066c6fa8c98d40e8098dcad231c99451
Asia <ziola.jm@gmail.com>
2023-09-25 15:15:24 +0200

    fix: InjectedEthereumSigner - address computed incorrectly

--- warp-contracts-plugin-signature/src/web/evm/InjectedEthereumSigner.ts
+++ warp-contracts-plugin-signature/src/web/evm/InjectedEthereumSigner.ts
@@ -1,8 +1,8 @@
 import { InjectedEthereumSigner as ArbundlesInjectedEthereumSigner } from 'arbundles';
-import { Wallet } from 'ethers';
+import { utils } from 'ethers';
 
 export class InjectedEthereumSigner extends ArbundlesInjectedEthereumSigner {
   async getAddress() {
-    return new Wallet(this.publicKey).getAddress();
+    return utils.computeAddress(this.publicKey);
   }
 }
