0aaa98417bb5db56558b99e74534c402d6212c39
Tadeuchi <tadeuszwl@gmail.com>
2024-03-14 16:18:56 +0100

    feat: beaver fever

--- warp-contracts-plugin-quickjs/src/eval/QuickJsEvaluator.ts
+++ warp-contracts-plugin-quickjs/src/eval/QuickJsEvaluator.ts
@@ -1,5 +1,6 @@
 import { QuickJSContext } from 'quickjs-emscripten';
 import { LoggerFactory } from 'warp-contracts';
+import { PNG } from 'pngjs';
 
 export class QuickJsEvaluator {
   private readonly logger = LoggerFactory.INST.create('QuickJsEvaluator');
@@ -16,6 +17,20 @@ export class QuickJsEvaluator {
     this.vm.setProp(this.vm.global, 'console', consoleHandle);
     consoleHandle.dispose();
     logHandle.dispose();
+  }
+
+  evalPngJS() {
+    const parseHandle = this.vm.newFunction('parse', (arg) => {
+      const png = PNG.sync.write(this.vm.dump(arg));
+      // vm.newArrayBuffer doesn't work as expected, so we return string
+      // https://github.com/justjake/quickjs-emscripten/issues/167
+      return this.vm.newString(png.toString('hex'));
+    });
+    const pngHandle = this.vm.newObject();
+    this.vm.setProp(pngHandle, 'parse', parseHandle);
+    this.vm.setProp(this.vm.global, 'PNG', pngHandle);
+    pngHandle.dispose();
+    parseHandle.dispose();
   }
 
   evalGlobalsCode(globalsCode: string) {
